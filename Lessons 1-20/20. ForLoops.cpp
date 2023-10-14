ef5452b26de8bff60f4befb63da152452b841d34
MrPink <105826290+MrPinksWorkshop@users.noreply.github.com>
2023-10-14 15:43:22 -0400

    Add files via upload

---
+++ Lessons 1-20/20. ForLoops.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+// 20. For loops
+// execute a block of code a specified amount of times
+// for loops tend to be beter in situations where you only need to repeat a certain amount times
+
+int main(){
+
+    // up to 3 statements within a for loop
+    for(int i = 10; i >= 0; i--){
+        std::cout << i << '\n';
+    }
+    std::cout << "HAPPY THANKSGIVING!\n";
+
+    return 0;
+}
