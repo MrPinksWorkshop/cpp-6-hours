d8e6a1eaf7ff148400f7cd8a608341249aa24646
MrPink <105826290+MrPinksWorkshop@users.noreply.github.com>
2023-10-14 15:34:17 -0400

    Add files via upload

---
+++ Lessons 1-20/6. ArithemeticOperators.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <ctime>
+#include <iomanip>
+
+// 6. Arithmetic operators
+// Arithmetic operators = return the result of a specific
+//                         arithmetic operation (+ - * /)
+
+//parenthesis
+//multiplication & division
+//addition & subtraction
+
+int main(){
+
+    int students = 20;
+
+    //students = students + 1;
+    //students+=1;
+    //students++;
+
+
+    //students= students - 1;
+    //students-=1;
+    //students--;
+
+    //students = students * 2;
+    //students*2=;
+
+    //students = students / 2;
+    //students/=3;
+
+    int remainder = students % 3;
+
+    std::cout << remainder;
+    
+    return 0;
+}
