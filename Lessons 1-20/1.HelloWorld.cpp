eccb59b2041b54bfd02a5cd8dc873a34c7990a04
MrPink <105826290+MrPinksWorkshop@users.noreply.github.com>
2023-10-14 15:25:48 -0400

    Create 1.HelloWorld.cpp

---
+++ Lessons 1-20/1.HelloWorld.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+
+// Why you need to learn C++ → Fast
+// advanced graphics apps (unreal engine)
+// embedded systems
+// video games
+
+
+// trade-off: high level language → easy to understand but slow
+
+// What do you need?
+// text editor
+// compiler (source code → machine instructions)
+ 
+
+
+// #include <iostream>
+
+// iostream is a header file that contains functions for basic input and output operations
+
+// int main(){      // invoke the main function
+//     std::cout << "Hello World!";  // standard character output
+//     return 0; 
+// }
+
+
+// 1. Hello World
+// int main(){
+
+//     //This is a comment
+//     /*
+//         This
+//         is
+//         a
+//         multi-line
+//         comment
+//     */
+
+//     std::cout << "I like pizza!" << std::endl; // add a semicolon to let the compiler know that the snippet is done
+//     std::cout << "It's really good!" << std::endl; // '\n' also works performance-wise
+//     return 0; 
+// }
